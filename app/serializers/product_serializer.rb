24f275f6029d7291fe0adc46495c0584ce32a198
Sandesh000 <sandesh.choudhary@taskcraft.in>
2022-11-10 16:38:18 +0530

    image

--- app/serializers/product_serializer.rb
+++ app/serializers/product_serializer.rb
@@ -24,14 +24,20 @@ class ProductSerializer < ActiveModel::Serializer
   #   end
   def images
     host = Rails.application.routes.default_url_options[:host] = "https://litoo.herokuapp.com/"
+    # host = Rails.application.routes.default_url_options[:host] = "http://localhost:3000/"
+
     if object.images.attached?
       object.images.map { |img|
-          {
-            id: img.id, filename: img.filename,
-            url: Rails.application.routes.url_helpers.rails_blob_path(img),
-            type: img.blob.content_type.split('/')[0]
-          }
-        }
+          # {
+          #   id: img.id, filename: img.filename,
+          #   url: Rails.application.routes.url_helpers.rails_blob_path(img),
+          #   type: img.blob.content_type.split('/')[0]
+          # }
+      
+          host + Rails.application.routes.url_helpers.rails_blob_path(img, only_path: true)
+        
+
+      }
       # host + Rails.application.routes.url_helpers.rails_blob_path(object.images, only_path: true)
     else
       "null"
