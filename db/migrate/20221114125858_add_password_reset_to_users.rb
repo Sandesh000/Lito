b48f9e00b1c682b6d5b88b57d0867d9f141debe7
Sandesh000 <sandesh.choudhary@taskcraft.in>
2022-11-15 17:01:22 +0530

    add_phone

--- db/migrate/20221114125858_add_password_reset_to_users.rb
+++ db/migrate/20221114125858_add_password_reset_to_users.rb
@@ -2,6 +2,6 @@ class AddPasswordResetToUsers < ActiveRecord::Migration[7.0]
   def change
     add_column :users, :password_reset_token, :string
     add_column :users, :password_reset_sent_at, :datetime
-    # add_column :users, :phone_number, :integer, limit: 8
+    add_column :users, :phone_number, :integer, limit: 8
   end
 end
