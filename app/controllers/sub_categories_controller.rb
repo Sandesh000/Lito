a90414665a6bf18d5c3a64f7c4d1d1c178bd19d7
Sandesh000 <sandesh.choudhary@taskcraft.in>
2022-11-22 11:27:46 +0530

    rent_sub

--- app/controllers/sub_categories_controller.rb
+++ app/controllers/sub_categories_controller.rb
@@ -9,20 +9,51 @@ class SubCategoriesController < ApplicationController
   def show
     # debugger
     @category = Category.find(params[:category_id])
-    @sub_category = @category.sub_categories.find(params[:id])
-    @products = @sub_category.products.all
+  #   @sub_category = @category.sub_categories.find(params[:id])
+  #   @products = @sub_category.products.all
+  #     data = []
+  #      @products.each do |product|
+  #     products = {}
+  #     products[:product_type] = product.product_type
+    
+  #     data << products
+  # end
+  #   render json: {
+		# 		data: data
+		# 	}, status: :ok
+
+
+  if @category.name == "Rent"
+    @rent_sub_category = @category.rent_sub_categories.find(params[:id])
+    @rent_products = @rent_sub_category.rent_products.all
     # host = Rails.application.routes.default_url_options[:host] = "https://litoo.herokuapp.com/"
     # @All_products = Product.all
       data = []
-       @products.each do |product|
+    @rent_products.each do |rent_product|
+      rent_products = {}
+      rent_products[:product_type] = rent_product.product_type
+      # products[:images] =host + Rails.application.routes.url_helpers.rails_blob_path(product.images.first, only_path: true)
+      data << rent_products
+    end
+    render json: {
+        data: data
+      }, status: :ok
+  else
+    @sub_category = @category.sub_categories.find(params[:id])
+    @products = @sub_category.products.all
+      data = []
+    @products.each do |product|
       products = {}
       products[:product_type] = product.product_type
-      # products[:images] =host + Rails.application.routes.url_helpers.rails_blob_path(product.images.first, only_path: true)
+    
       data << products
-  end
+    end
     render json: {
-				data: data
-			}, status: :ok
+        data: data
+      }, status: :ok
+  end
+
+
 
   end
   
